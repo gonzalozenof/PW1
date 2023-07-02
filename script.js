468b2b4a461c3b7c663ac71c436e72e54a808c26
gonzalozenof <gonzalozenof@gmail.com>
2023-07-02 18:40:11 -0300

    Cambio de logos y java con favoritos de cards

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
+function toggleFavorite(button) {
+  var card = button.closest('.card');
+  card.classList.toggle('favorito');
 
-var mensaje1 = "Hola";
-var mensaje2 = " Mundo";
-var mensaje = mensaje1 + mensaje2; 
+  // Actualizar el contador de favoritos
+  var favoritesCount = document.querySelector('.favorites-count');
+  var favoriteCards = document.querySelectorAll('.card.favorito');
+  favoritesCount.textContent = favoriteCards.length;
 
-document.write(mensaje);
+  // Actualizar la lista de previsualización de favoritos
+  var favoritesPreviewList = document.querySelector('.favorites-preview-list');
+  favoritesPreviewList.innerHTML = '';
 
+  favoriteCards.forEach(function(card) {
+    var cardTitle = card.querySelector('.card-title').textContent;
+    var previewItem = document.createElement('li');
+    previewItem.textContent = cardTitle;
+    favoritesPreviewList.appendChild(previewItem);
+  });
 
-function checkAge(age) {
-    if (age > 18) {
-      return true;
-    } else {
-      return confirm('¿Estas autorizado?');
-    }
-  }
-  
-  let age = prompt('¿Qué edad tienes?', 18);
-  
-  if ( checkAge(age) ) {
-    alert( 'Acceso permitido' );
+  // Mostrar/ocultar la previsualización de favoritos
+  var favoritesPreview = document.querySelector('.favorites-preview');
+  if (favoriteCards.length > 0) {
+    favoritesPreview.style.display = 'block';
   } else {
-    alert( 'Acceso denegado' );
+    favoritesPreview.style.display = 'none';
   }
+}
+
+var favoritesCount = document.querySelector('.favorites-count');
+favoritesCount.addEventListener('click', function() {
+  var favoritesPreview = document.querySelector('.favorites-preview');
+  if (favoritesPreview.style.display === 'none') {
+    favoritesPreview.style.display = 'block';
+  } else {
+    favoritesPreview.style.display = 'none';
+  }
+});
